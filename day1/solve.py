1384bf51ca2da612e0423c6c786025feb3f78446
Log_s <leo951206@gmail.com>
2021-12-05 23:35:48 +0100

    Day1

---
+++ day1/solve.py
@@ -0,0 +1,26 @@
+# Load inputs
+depths = [int(x) for x in open("input.txt", "r").readlines()]
+
+
+### Exercise 1 ###
+increases = 0
+
+for i in range(1, len(depths)):
+    if depths[i] > depths[i-1]:
+        increases += 1
+
+print("[+] Exercise 1:",increases)
+### ########## ###
+
+### Exercise 1 ###
+increases = 0
+last = sum([depths[0], depths[1], depths[2]])
+
+for i in range(1, len(depths)-2):
+    s = sum([depths[i], depths[i+1], depths[i+2]])
+    if s > last:
+        increases += 1
+    last = s
+
+print("[+] Exercise 2:",increases)
+### ########## ###
